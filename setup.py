dbb6dbde0a77fb6fac98f7f5327cb50872eb7b0c
auto3000 <auto3000@users.noreply.github.com>
2017-06-14 06:58:33 +0200

    setup: create installation script

---
+++ setup.py
@@ -0,0 +1,14 @@
+import sys
+from setuptools3 import setup
+setup(
+ name = "pedalpii",
+ version = "1.0",
+ py_modules = [ 'pedalpII' ]
+ author="auto3000",
+ author_email = "auto3000@users.noreply.github.com",
+ description = "Physical (button/LCD) and console interfaces for the pedalpii.",
+ license = "GPLv3",
+ keywords= "pedalpii",
+ url = "https://github.com/auto3000/pedalpii",
+)
+
